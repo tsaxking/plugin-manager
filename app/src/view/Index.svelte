0fcd3651fa77aeae724c197ab5fb3173800e22f8
tsaxking <taylorking206@u.boisestate.edu>
2024-05-05 19:12:10 -0600

    invoke frontend

--- app/src/view/Index.svelte
+++ app/src/view/Index.svelte
@@ -1,9 +1,16 @@
-<script lang="ts">
-    import { onMount } from 'svelte';
-    let message = '';
-    onMount(() => {
-        message = 'Hello World!';
-    });
-</script>
-
-<h1>{message}</h1>
+<script>
+    import { invoke } from '@tauri-apps/api/tauri'
+  
+    let name = ''
+    let greetMsg = ''
+  
+    async function greet() {
+      greetMsg = await invoke('greet', { name })
+    }
+  </script>
+  
+  <div>
+    <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+    <button on:click="{greet}">Greet</button>
+    <p>{greetMsg}</p>
+  </div>
