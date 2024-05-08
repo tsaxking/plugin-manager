e2b51aa499e3f3de6793c0a563de60bc48110e92
Berint Moffett <berint.moffett@gmail.com>
2024-05-08 15:35:40 -0600

    rebase main

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,14 @@
+pub mod save_load;
+
 #[tauri::command]
-pub fn get_effects_state() -> String {
-    serde_json::to_string(&crate::tests::test_state_schema::get()).unwrap()
+pub fn get_effects_state() -> Result<String, crate::AppStateError> {
+    let Some(rw_lock) = crate::APP_STATE
+        .get() else {
+            return Err(crate::AppStateError::Uninitialized);
+        };
+    let Ok(guard) = rw_lock.read() else {
+        return Err(crate::AppStateError::Poisoned);
+    };
+    Ok(serde_json::to_string(&*guard)?)
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use plugin_manager::commands;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    plugin_manager::init_app_state(vec![]);
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![commands::get_effects_state])
-        .run(tauri::generate_context!())
-        .expect("unable to run tauri application");
+        .invoke_handler(tauri::generate_handler![
+            commands::get_effects_state,
+            commands::save_load::save,
+            commands::save_load::load,
+        ])
+        .run(tauri::generate_context!())?;
+
+    Ok(())
 }
