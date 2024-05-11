fb59e392990b47e06befa05b971964ad7934a651
shakesbeare <75107188+shakesbeare@users.noreply.github.com>
2024-05-11 11:00:37 -0600

    restructure project to use Cargo Workspaces (#35)
    * enormous restructure
    
    * fix rust.yml missing space
    
    * revert rename "app/" to "ui/"
    
    * export "ruckus_rack" as "ruckus_lib::rack"
    
    * rename ruckus to ruckus_app;
    rename ruckus_lib to ruckus;
    move local crates to crates/;
    
    * ruckus (lib) combined with ruckus_app in root folder;
    cargo lint alias for cargo clippy --manifest-path Cargo.toml -- -D
    warnings;
    
    * fix cargofmt
    
    * local dependencies built as dynamic library to further decrease compile
    time where possible
    
    * add rlib crate type
    
    * rustflags -C prefer-dynamic
    
    * force rustflags in CI
    
    * revert dylib
    
    * rename ruckus to pm -> name change not final

---
+++ crates/pm_rack/src/lib.rs
@@ -0,0 +1,131 @@
+#[cfg(debug_assertions)]
+pub mod dev;
+
+use std::sync;
+
+pub static APP_STATE: sync::OnceLock<sync::RwLock<AppState>> = sync::OnceLock::new();
+pub static PLAY_TX: sync::OnceLock<sync::Mutex<sync::mpsc::Sender<usize>>> =
+    sync::OnceLock::new();
+
+#[derive(thiserror::Error, serde::Serialize, Debug)]
+pub enum AppStateError {
+    #[error("APP_STATE was not properly initialized at program start")]
+    Uninitialized,
+
+    #[error("APP_STATE RwLock was poisoned. Did a thread panic?")]
+    Poisoned,
+
+    #[error("APP_STATE could not be serialized into JSON")]
+    SerializeError {
+        #[from]
+        #[serde(skip)]
+        source: serde_json::Error,
+    },
+}
+
+pub type AppState = Vec<RackItem>;
+
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct RackItem {
+    pub id: String,
+    pub note: String,
+    pub point: (u8, u8),
+    pub width: u8,
+    pub color: String,
+    pub title: String,
+    pub io: Io,
+    pub routing: Routing,
+}
+
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct Io {
+    pub audio: IoInner,
+    pub midi: IoInner,
+    pub control: IoInner,
+}
+
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct IoInner {
+    pub input: Vec<String>,
+    pub output: Vec<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct Routing {
+    pub audio: Vec<String>,
+    pub midi: Vec<String>,
+    pub control: Vec<String>,
+}
+
+pub fn init_app_state(state: AppState) {
+    APP_STATE
+        .set(sync::RwLock::new(state))
+        .expect("Critical Error: Could not set APP_STATE");
+}
+
+pub fn init_play_tx(tx: sync::mpsc::Sender<usize>) {
+    PLAY_TX
+        .set(sync::Mutex::new(tx))
+        .expect("Critical Error: Could not set PLAY_TX");
+}
+
+mod tests {
+
+    #[cfg(test)]
+    #[derive(Debug)]
+    struct IgnoreWhitespace<'a> {
+        string: &'a str,
+    }
+
+    #[cfg(test)]
+    impl<'a> PartialEq for IgnoreWhitespace<'a> {
+        fn eq(&self, other: &Self) -> bool {
+            let mut self_iter = self.string.chars();
+            let mut other_iter = other.string.chars();
+
+            while let (Some(mut self_c), Some(mut other_c)) =
+                (self_iter.next(), other_iter.next())
+            {
+                while self_c.is_whitespace() {
+                    let opt_c = self_iter.next();
+                    if opt_c.is_some() {
+                        self_c = opt_c.unwrap();
+                    }
+                }
+
+                while other_c.is_whitespace() {
+                    let opt_c = other_iter.next();
+                    if opt_c.is_some() {
+                        other_c = opt_c.unwrap();
+                    }
+                }
+
+                if self_c != other_c {
+                    return false;
+                }
+            }
+
+            true
+        }
+    }
+
+    #[test]
+    fn serialize_app_state() {
+        let schema = crate::dev::test_state_schema::get();
+        let actual = serde_json::to_string(&schema).unwrap().to_string();
+        let expected = std::fs::read_to_string("./src/dev/serialized.json").unwrap();
+
+        let left = IgnoreWhitespace { string: &actual };
+        let right = IgnoreWhitespace { string: &expected };
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn deserialize_app_state() {
+        use crate::AppState;
+        let json = std::fs::read_to_string("./src/dev/serialized.json").unwrap();
+        let actual: AppState = serde_json::from_str(&json).unwrap();
+        let expected = crate::dev::test_state_schema::get();
+        assert_eq!(actual, expected);
+    }
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,23 +1,23 @@
 pub mod save_load;
 
 #[tauri::command]
-pub fn get_effects_state() -> Result<String, crate::AppStateError> {
-    let Some(rw_lock) = crate::APP_STATE.get() else {
-        return Err(crate::AppStateError::Uninitialized);
+pub fn get_effects_state() -> Result<String, pm_rack::AppStateError> {
+    let Some(rw_lock) = pm_rack::APP_STATE.get() else {
+        return Err(pm_rack::AppStateError::Uninitialized);
     };
     let Ok(guard) = rw_lock.read() else {
-        return Err(crate::AppStateError::Poisoned);
+        return Err(pm_rack::AppStateError::Poisoned);
     };
     Ok(serde_json::to_string(&*guard)?)
 }
 
 #[tauri::command]
-pub fn toggle_playback() -> Result<(), crate::AppStateError> {
-    let Some(mutex) = crate::PLAY_TX.get() else {
-        return Err(crate::AppStateError::Uninitialized);
+pub fn toggle_playback() -> Result<(), pm_rack::AppStateError> {
+    let Some(mutex) = pm_rack::PLAY_TX.get() else {
+        return Err(pm_rack::AppStateError::Uninitialized);
     };
     let Ok(guard) = mutex.lock() else {
-        return Err(crate::AppStateError::Poisoned);
+        return Err(pm_rack::AppStateError::Poisoned);
     };
     guard.send(0).unwrap();
     Ok(())
