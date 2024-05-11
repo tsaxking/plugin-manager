746cd33df4d1eb963f930cc4db571ec73c927191
tsaxking <taylorking206@u.boisestate.edu>
2024-05-10 19:55:25 -0600

    move commands over

--- src/console/mod.rs
+++ src/console/mod.rs
@@ -34,6 +34,37 @@ impl Cli {
 pub fn commands() -> Cli {
     let cli = Cli::new();
 
-    cli.add(String::from("ping"), &|_: Vec<&str>| String::from("pong"))
+    cli.add(String::from("ping"), &|_| String::from("pong"))
         .add(String::from("echo"), &|args: Vec<&str>| args.join(" "))
+        .add(String::from("help"), &|_| {
+            let mut output = String::new();
+            output.push_str("\nAvailable commands: \n");
+            for cmd in [
+                "help",
+                "greet",
+                "toggle_playback",
+                "save <filename",
+                "load <filename>",
+            ] {
+                let text = format!(":: {}\n", cmd);
+                output.push_str(&text);
+            }
+            output
+        })
+        .add(String::from("greet"), &|_| String::from("Hello World"))
+        .add(String::from("save"), &|args| {
+            let path = args.join(" ");
+            crate::commands::save_load::save(path.clone()).unwrap();
+            format!("Saving APP_STATE to {}", path)
+        })
+        .add(String::from("load"), &|args| {
+            let path = args.join(" ");
+            crate::commands::save_load::load(path.clone()).unwrap();
+            format!("Loading APP_STATE from {}", path)
+        })
+        .add(String::from("toggle_play"), &|_| {
+            crate::commands::toggle_playback().unwrap();
+            "Playback toggled".to_string()
+        })
+        .add(String::from(""), &|_| String::new())
 }
