370284491b3750a35758860f102f9b41fed55076
tsaxking <taylorking206@u.boisestate.edu>
2024-05-13 20:49:21 -0600

    ref issue

--- src/bin/audio_outputs.rs
+++ src/bin/audio_outputs.rs
@@ -1,14 +1,33 @@
 use cpal::traits::{DeviceTrait, HostTrait};
+use pm::console::commands;
 
 
 fn main() -> anyhow::Result<()> {
     let hosts = cpal::available_hosts();
+
+    let mut map = std::collections::HashMap::new();
+
     for host_id in hosts {
         let host = cpal::host_from_id(host_id)?;
-        for device in host.devices()? {
-            println!("  {}", device.name()?);
+        let mut i = 0;
+        for device in host.output_devices()? {
+            println!("[{}]: {:?}", i, device.name());
+            map.insert(i, device);
+            i += 1;
         }
     }
+
+    commands().add(
+        String::from("audio"), &|args| {
+            let i = args.first().unwrap().parse::<usize>().unwrap();
+            let device = map.get(&i);
+
+            if let Some(d) = device {
+                format!("Selected device: {:?}", d.name())
+            } else {
+                String::from("Device not found")
+            }
+        });
 
     Ok(())
 }
