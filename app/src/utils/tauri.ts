46de355004c5050db7a1c3b823763e7089ad5360
tsaxking <taylorking206@u.boisestate.edu>
2024-08-01 00:43:17 -0600

    start socket

--- app/src/utils/tauri.ts
+++ app/src/utils/tauri.ts
@@ -2,12 +2,30 @@ import { invoke } from '@tauri-apps/api/tauri';
 import { attemptAsync } from './check';
 
 
-export const call = (event: string, payload: Record<string, unknown>) => {
-    return attemptAsync(async () => {
-        return await invoke('global', {
-            data: {
-                [event]: payload,
-            }
-        });
+let id = 0;
+export const call = <T = unknown>(event: string, payload: Record<string, unknown>) => {
+    return attemptAsync(() => {
+        return new Promise((res, rej) => {
+            invoke('global', {
+                [event]: {
+                    id_: id,
+                    ...payload,
+                }
+            })
+                .catch(e => rej(e));
+
+            socket.once(event, (data: {
+                id: number;
+                data: unknown
+            }) => {
+                if (data.id === id) {
+                    res(data.data as T);
+                }
+            });
+
+            id++;
+        })
     });
 }
+
+const socket: any = {};
